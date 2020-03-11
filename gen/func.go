a0fe3344a73e7d6e742ae82d843fedb6c453e5f8
lhlyu <2438211370@qq.com>
2020-03-11 09:22:38 +0800

    mysql to gorm model

--- gen/func.go
+++ gen/func.go
@@ -1,17 +1,55 @@
 package gen
 
 import (
-	"fmt"
 	"github.com/lhlyu/got/db/core"
 	"github.com/lhlyu/yutil/v2"
 )
 
 const g = `
-func (%s) Name() string {
-	return "%s"
+// 获取
+func (this *{{.}}) Get() error{
+    if this.Id == 0{
+        return MissPkErr
+    }
+    return common.DB.First(this,this.Id).Error
 }
+
+// 分页查询
+func (this *{{.}}) Query(rs interface{},page *Page,whr map[string]interface{}, order string) error{
+    var total int
+    if err := common.DB.Model(this).Where(whr).Count(&total).Error;err != nil{
+        return err
+    }
+    page.SetTotal(total)
+    return common.DB.Where(whr).Offset(page.StartRow).Limit(page.PageSize).Order(order).Find(rs).Error
+}
+
+// 添加
+func (this *{{.}}) Add() error{
+    return common.DB.Create(this).Error
+}
+
+// 删除
+func (this *{{.}}) Del() error{
+    if this.Id == 0{
+        return MissPkErr
+    }
+    return common.DB.Unscoped().Delete(this).Error
+}
+
+// 更新
+func (this *{{.}}) Update(whr map[string]interface{}) error{
+    if this.Id == 0{
+        return MissPkErr
+    }
+    if whr == nil{
+        return common.DB.Model(this).Updates(this).Error
+    }
+    return common.DB.Model(this).Updates(whr).Error
+}
+
 `
 
 var GORM_CURD = func(tab *core.Table) string {
-	return fmt.Sprintf(g, yutil.String.BigCamelCase(tab.Name), tab.Name)
+	return yutil.String.TemplateParse(g, yutil.String.BigCamelCase(tab.Name))
 }
